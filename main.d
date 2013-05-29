c17777e9d019d3da310eb14bd1b9340604fbf13a
Rikki Guy <rikki.guy@gmail.com>
2013-05-29 13:34:29 +0100

    Refactored sidesize to runtime param. Test case.

--- main.d
+++ main.d
@@ -1,23 +1,36 @@
 import std.stdio;
+import std.exception : enforce;
 
 void main(string[] args)
 {
-    writeln("Initialising...");
+    writeln("Initialising World...");
+    
+    World myWorld = World.generateEmpty();
 }
 
 struct Square {
-    float height;
+    float height = 0.0f;
 }
 
 
 interface World {
     Square get(int x, int y);
     void set(int x, int y, Square square);
+    
+    static World generateEmpty(int sidesize = 128) {
+        return new WorldImpl(sidesize);
+    }
 }
 
-class WorldImpl(int sidesize) : World {
+class WorldImpl : World {
     
-    private Square[sidesize*sidesize] squares;
+    private this(uint sidesize) {
+        this.sidesize = sidesize;
+        squares = new Square[sidesize * sidesize];
+    }
+    
+    immutable private uint sidesize;
+    private Square[] squares;
     
     override public Square get(int x, int y) {
         enforce(x < sidesize && y < sidesize, "Square out of bounds!");
