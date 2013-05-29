a60bd1b526c23d9b4262303477c442b49442a88d
Rikki Guy <rikki.guy@gmail.com>
2013-05-29 13:27:04 +0100

    added World interface & WorldImpl

--- main.d
+++ main.d
@@ -4,3 +4,28 @@ void main(string[] args)
 {
     writeln("Initialising...");
 }
+
+struct Square {
+    float height;
+}
+
+
+interface World {
+    Square get(int x, int y);
+    void set(int x, int y, Square square);
+}
+
+class WorldImpl(int sidesize) : World {
+    
+    private Square[sidesize*sidesize] squares;
+    
+    override public Square get(int x, int y) {
+        enforce(x < sidesize && y < sidesize, "Square out of bounds!");
+        return squares[y * sidesize + x];
+    }
+    
+    override public void set(int x, int y, Square square) {
+        enforce(x < sidesize && y < sidesize, "Square out of bounds!");
+        squares[y * sidesize + x] = square;
+    }
+}
